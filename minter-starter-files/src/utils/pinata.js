4f86f73e7ffc5e2a8df1e7de68cec527410c4b95
FAME\mustafa <mustafaonure@gmail.com>
2022-08-12 00:46:00 +0300

    NFT

--- minter-starter-files/src/utils/pinata.js
+++ minter-starter-files/src/utils/pinata.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
-const key = process.env.REACT_APP_PINATA_KEY;
-const secret = process.env.REACT_APP_PINATA_SECRET;
+const key = "9db009b7a4f22b2ef295";
+const secret = "056f8fa22a7939aaa91f8dc885ad896cc4c5799f3518cdf0f3fd983589ab5462";
+
 const axios = require('axios');
 
-export const pinJSONToIPFS = async(JSONBody) => {
+export const pinJSONToIPFS = async (JSONBody) => {
     const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+    //making axios POST request to Pinata ⬇️
     return axios
         .post(url, JSONBody, {
             headers: {
@@ -13,10 +15,10 @@ export const pinJSONToIPFS = async(JSONBody) => {
             }
         })
         .then(function (response) {
-           return {
-               success: true,
-               pinataUrl: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
-           };
+            return {
+                success: true,
+                pinataUrl: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+            };
         })
         .catch(function (error) {
             console.log(error)
@@ -24,6 +26,6 @@ export const pinJSONToIPFS = async(JSONBody) => {
                 success: false,
                 message: error.message,
             }
-           
+
         });
 };
